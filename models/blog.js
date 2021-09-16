ef574458a8ac5020ac0ec5c87fff3859ef3726c6
Gray007 <cmzlelicanin@gmail.com>
2021-09-16 09:55:06 +0200

    blog list - 4.2

--- models/blog.js
+++ models/blog.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose')
-// const uniqueValidator = require('mongoose-unique-validator')
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -19,8 +6,6 @@ const blogSchema = new mongoose.Schema({
     url: String,
     likes: Number
 })
-
-// personSchema.plugin(uniqueValidator)
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
